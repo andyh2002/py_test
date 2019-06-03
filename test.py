90142532340b82fc50bec6384235a3c88b8c19fc
xrobinson <andyh200@qq.com>
2019-06-03 19:33:21 +0800

    add new files

---
+++ test.py
@@ -0,0 +1,12 @@
+#! usr/bin/env python
+import datetime
+st="00:00:00"
+et="00:00:10"
+st_time = datetime.datetime.strptime(st,'%H:%M:%S')
+et_time = datetime.datetime.strptime(et,'%H:%M:%S')
+type(st_time)
+type(et_time)
+
+print(st_time)
+print(et_time)
+print((et_time-st_time).seconds)
