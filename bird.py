d8f7e2c63d5a81b61c28149c949110b86e4cf4d1
xrobinson <andyh2002@qq.com>
2021-01-02 21:20:24 +0800

    2021年1月2日21:20:17

--- bird.py
+++ bird.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 class Bird:
     def __init__(self):
-            self.hungry = True
+        self.hungry = True
 
     def eat(self) -> object:
         if self.hungry:
             print("Aaaah...")
-            self.hungry = False
+            self.hungry = False  # eat之后将hungry改成False
         else:
             print("No. Thanks!")
 
 
 class SongBird(Bird):
     def __init__(self):
-        Bird.__init__(self)         # 调用超类未绑定的构造方法
+        Bird.__init__(self)  # 调用超类未绑定的构造方法
         self.sound = 'Squawk!'
 
     def sing(self):
         print(self.sound)
+
 
 sb = SongBird()
 sb.sing()
